c60ae0fd21f15d829b23e5c2382933f326a7bd45
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-06-19 15:31:34 -0300

    feat: add header (make responsiveness)

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,5 +2,9 @@ import Header from "../../components/Header/Header";
 import "./index.scss";
 
 export default function Home({ data }) {
-  return <div>Home</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
