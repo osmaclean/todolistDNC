8b43818f01851e2d8c9ed3d6ef146a74e484d036
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-06-22 09:18:19 -0300

    feat: added modal functionality, now styling

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,10 +1,19 @@
 import "./index.scss";
 import trash from "../../assets/trash.png";
 import pencil from "../../assets/pencil.png";
+import { useState } from "react";
 
 export default function Tasks({ data }) {
+  const [showModalEdit, setShowModalEdit] = useState(false);
+  const [showModalTrash, setShowModalTrash] = useState(false);
   return (
-    <div className="containerTable">
+    <div
+      className="containerTable"
+      onClick={() => {
+        if (showModalEdit) setShowModalEdit(false);
+        if (showModalTrash) setShowModalTrash(false);
+      }}
+    >
       <table id="tfhover" className="tftable">
         <tr>
           <th>Tarefa</th>
@@ -18,8 +27,23 @@ export default function Tasks({ data }) {
               <input type="checkbox" />
             </td>
             <td>
-              <img src={pencil}></img>
-              <img src={trash}></img>
+              <img onClick={() => setShowModalEdit(true)} src={pencil}></img>
+              <img onClick={() => setShowModalTrash(true)} src={trash}></img>
+
+              {showModalEdit && (
+                <div className="modal__container">
+                  <div className="modal__container__content">
+                    <h1>Teste</h1>
+                  </div>
+                </div>
+              )}
+              {showModalTrash && (
+                <div className="modal__container">
+                  <div className="modal__container__content">
+                    <h1>Teste 2</h1>
+                  </div>
+                </div>
+              )}
             </td>
           </tr>
         ))}
